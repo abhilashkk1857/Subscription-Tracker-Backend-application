38ecb90a65f03704eca5267c23260f931cbd695f
Abhilash K K <abhilashkk1857@gmail.com>
2025-03-14 22:17:49 +0530

    connect with the database (MongoDB Atlas)

---
+++ src/database/mongodb.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_URI as uri} from "../config/env.js";
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(uri)
+        console.log("DB connected successfully!")
+    }
+    catch(err) {
+        console.log(`Unable to connect with the Database.${err}`)
+        process.exit(1)
+    }
+}
+
+
+export default connectDB
