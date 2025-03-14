8cf5d2c700121fe353b3f744123b77130e771972
Abhilash K K <abhilashkk1857@gmail.com>
2025-03-14 22:23:25 +0530

    updated the server.js to deal with db-connection and routes.

--- src/server.js
+++ src/server.js
@@ -1,17 +1,29 @@
-import express from "express"
-import { PORT as port } from "./config/env.js"
+import express from "express";
+import { PORT as port } from "./config/env.js";
+import connectDB from "./database/mongodb.js";
 
-const app = express()
+import userRouter from "./routes/user.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import authRouter from "./routes/auth.routes.js";
+
+
+const app = express();
+
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions",subscriptionRouter);
 
 
 app.get("/", (req,res) => {
-    res.status(200).json({messsage:"Welcom to the root route."})
-})
+    res.status(200).json({messsage:"Welcome to the Subscription Tracker API!."});
+});
 
 
 
 
 app.listen(port, () => {
     console.log(`The server is listening to the port ${port}`);
-})
+    connectDB()     //connect to the DB.
+});
 
