6f78a844a375093ba6fa100f456fcb537660dbaf
Abhilash K K <abhilashkk1857@gmail.com>
2025-03-15 22:18:28 +0530

    A new middleware is made to handle the errors globally.

---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,35 @@
+const errorHandler = (err,req,res,next) => {
+    try{
+        let error = {...err};
+        error.message = err.message;
+
+        // Mongoose bad ObjectId
+        if(err.name === "CastError"){
+            const message = "Resource Not Found!";
+            error = new Error(message);
+            error.statusCode = 404; 
+        }
+
+        // Mongoose duplicate key
+        if(err.statusCode === 11000){
+            const message = "Duplicate Field value entered.";
+            error = new Error(message);
+            error.statusCode = 400;
+        }
+
+        // Mongoose Validation error
+        if(err.name === "ValidationError"){
+            const message = Object.values(err.error).map(val => val.message);
+            error = new Error(message.join(", "));
+            error.statusCode = 400;
+        }
+
+        res.status(error.statusCode || 500).json({success:false, error:error.message || "Internal Server Error!"});
+
+    }
+    catch(error){
+        next(error);
+    }
+};
+
+export default errorHandler;
